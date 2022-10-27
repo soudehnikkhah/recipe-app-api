fd83b8fb14c56dcf3f3956f8e73a444e15bd796a
Soudeh NikkhahAzad <soudeh.n.a@gmail.com>
2022-10-27 20:22:35 +0200

    this is commit from nikkhah

---
+++ app/app/calc.py
@@ -0,0 +1,6 @@
+def Add (x,y):
+    return x+y
+
+
+def subtract(x,y):
+    return y-x
---
+++ app/app/tests.py
@@ -0,0 +1,14 @@
+from django.test import SimpleTestCase
+from app import calc
+
+
+class calcTest(SimpleTestCase):
+    def test_add_numbers(self):
+        res=calc.Add(5,6)
+        self.assertEqual(res,11)
+
+
+    def test_subtract_numbers(self):
+        res=calc.subtract(10,15)
+        self.assertEqual(res,5)
+
